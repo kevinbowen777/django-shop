ddf88cef4b34f7a3321d47bc84495aa38837d377
Kevin Bowen <kevin.bowen@gmail.com>
2023-12-29 11:55:14 -0800

    Install sphinx-lint. Add to pre-commit. Resolves #86

--- docs/conf.py
+++ docs/conf.py
@@ -3,9 +3,9 @@ project = "django-shop"
 author = "Kevin Bowen"
 copyright = f"2023, {author}"
 #
-html_theme = 'furo'
-html_logo = 'django_24.png'
-html_title = 'django-shop'
+html_theme = "furo"
+html_logo = "django_24.png"
+html_title = "django-shop"
 extensions = [
-    'sphinx.ext.duration',
+    "sphinx.ext.duration",
 ]
